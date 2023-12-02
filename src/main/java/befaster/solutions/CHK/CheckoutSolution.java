be1147dccce47bf06ca211ecda1a7dfcb12a4536
sbx_user1051 <sbx_user1051@169.254.27.69>
2023-12-02 22:49:55 +0000

    Sat Dec 02 22:49:55 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -30,26 +30,49 @@ public class CheckoutSolution {
 
         Map<String, Long> itemByCount = items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        for ( Map.Entry<String, Long> entry : itemByCount.entrySet()){
-                String type = entry.getKey();
-                Long total = entry.getValue();
-                if("A".equals(type)){
-                    double q1 = (total / 5) * 200; // item for multiple of 5
-                    double q2 = ((total % 5) / 3) * 130; // item for multiple of 3
-                    double q3 = ((total % 5) % 3) * 50; // items for remaining
-                    sum[0] += q1 + q2 + q3;
-                }else if("B".equals(type)){
-                    sum[0] += (total/2)*45 + (total % 2)* 30;
-                }else if("C".equals(type)){
-                    sum[0] +=20*total;
-                }else if("D".equals(type)){
-                    sum[0] +=15*total;
-                }else if("E".equals(type)){
-                    sum[0] += (total / 2) * 40 + (total % 2) * 40 + (total / 2) * 30;
-                }
-        }
+        Long quantA = itemByCount.get("A");
+        double q1 = (quantA / 5) * 200; // item for multiple of 5
+        double q2 = ((quantA % 5) / 3) * 130; // item for multiple of 3
+        double q3 = ((quantA % 5) % 3) * 50; // items for remaining
+        double totA =  q1 + q2 + q3;
 
-        return  sum[0];
+        Long quantB = itemByCount.get("B");
+        double totB = (quantB/2)*45 + (quantB % 2)* 30;;
+
+        Long quantC = itemByCount.get("C");
+        double totC = 20 * quantC;
+
+        Long quantD = itemByCount.get("D");
+        double totD = 15 * quantD;
+
+        Long quantE = itemByCount.get("E");
+        double totE = (quantE / 2) * 40 + (quantE % 2) * 40 + (quantB / 2) * 30;
+
+        int count = (int) (totA + totB + totC + totD + totE);
+
+
+//        for ( Map.Entry<String, Long> entry : itemByCount.entrySet()){
+//                String type = entry.getKey();
+//                Long total = entry.getValue();
+//                if("A".equals(type)){
+//                    double q1 = (total / 5) * 200; // item for multiple of 5
+//                    double q2 = ((total % 5) / 3) * 130; // item for multiple of 3
+//                    double q3 = ((total % 5) % 3) * 50; // items for remaining
+//                    sum[0] += q1 + q2 + q3;
+//                }else if("B".equals(type)){
+//                    sum[0] += (total/2)*45 + (total % 2)* 30;
+//                }else if("C".equals(type)){
+//                    sum[0] += 20 * total;
+//                }else if("D".equals(type)){
+//                    sum[0] += 15 * total;
+//                }else if("E".equals(type)){
+//                    sum[0] += (total / 2) * 40 + (total % 2) * 40 + (total / 2) * 30;
+//                }
+//        }
+//        return  sum[0];
+
+        return count;
 
     }
 }
+
