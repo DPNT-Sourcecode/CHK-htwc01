bba329b3a8d57ccb80c160c7005d57303a5640b1
sbx_user1051 <sbx_user1051@169.254.47.165>
2023-12-02 20:30:10 +0000

    Sat Dec 02 20:30:10 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,6 +4,8 @@ import befaster.runner.SolutionNotImplementedException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CheckoutSolution {
@@ -16,10 +18,13 @@ public class CheckoutSolution {
         List<String> items = Arrays.asList(skus.split(", "));
         int sum[] = {0};
 
-        items.stream().collect(Collectors.groupingBy())
+        Map<String, Long> itemByCount = items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        items.fo
 
     }
 }
+
 
 
 
