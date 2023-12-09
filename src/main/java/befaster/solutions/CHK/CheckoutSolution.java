2c40c8a5c638df8cb24cd6ca04a12b403a64ad44
sbx_user1051 <sbx_user1051@169.254.29.221>
2023-12-09 17:26:03 +0000

    Sat Dec 09 17:26:03 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -120,7 +120,7 @@ public class CheckoutSolution {
             if(specialOfferoffer.getOfferForQuantityList().size() > 0){
                 int actualCount = count.intValue();
                 int totPrc = 0;
-                specialOfferoffer.getOfferForQuantityList().stream().sorted((o1, o2) -> o1.getOfferQuant() ).collect(Collectors.toList())
+                List<OfferForQuantity> collect = specialOfferoffer.getOfferForQuantityList().stream().sorted(Comparator.comparingInt(OfferForQuantity::getOfferQuant)).collect(Collectors.toList());
                 for (OfferForQuantity offer: specialOfferoffer.getOfferForQuantityList()) {
 
                     if(actualCount >= offer.getOfferQuant()) {
@@ -138,8 +138,3 @@ public class CheckoutSolution {
         return totalPrice[0];
     }
 }
-
-
-
-
-
